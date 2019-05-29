a5798f9edc12b3ab8c1b6a50d58eea967342af1f
Yuko70 <martin.masek10@gmail.com>
2019-05-29 10:50:42 +0200

    1 file modified

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 // Import stylesheets
 import './style.css';
 
-// Write Javascript code!
-// const appDiv = document.getElementById('app');
-// appDiv.innerHTML = `<h1>Flappy</h1>`;
+
+let cvsgame = document.getElementById("game");
+let ctxG = cvsgame.getContext("2d");
+
+let bird = new Image();
+let fg = new Image();
+let pipeUp = new Image();
+let pipeDown = new Image();
+let pipeMiddle= new Image();
 
 
-let canvasbg = document.getElementById("bg");
-let ctx = canvasbg.getContext("2d");
+start();
 
-let imageBG = new Image();
-imageBG.src = 'https://raw.githubusercontent.com/yuko70/tia-js-skuska/master/math_flappy_bg.png';
+function start() {
 
-ctx.drawImage(imageBG, 0, 0, canvasbg.width, canvasbg.height);
+  let canvasbg = document.getElementById("bg");
+  let ctx = canvasbg.getContext("2d");
+  let imageBG = new Image();
+  imageBG.src = 'https://raw.githubusercontent.com/yuko70/tia-js-skuska/master/math_flappy_bg.png';
+  ctx.drawImage(imageBG, 0, 0, canvasbg.width, canvasbg.height);
 
-// image.onload=function(){
-  
-// };
+
+
+
+}
