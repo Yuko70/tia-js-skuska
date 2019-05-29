16cfb820f858fb492dfd1150c6d90330858745b9
Yuko70 <martin.masek10@gmail.com>
2019-05-29 11:49:48 +0200

    1 file modified

--- index.js
+++ index.js
@@ -20,58 +20,48 @@ pipeUp.src = "https://raw.githubusercontent.com/yuko70/tia-js-skuska/master/pipe
 pipeDown.src = "https://raw.githubusercontent.com/yuko70/tia-js-skuska/master/pipeDown.png";
 pipeMiddle.src = "https://raw.githubusercontent.com/yuko70/tia-js-skuska/master/pipeMiddle.png";
 
-// ctxG.drawImage(bird, bX, bY, 30, 30);
-
-
 let score = 0;
 let space = 150;
 
 let bX = 50;
 let bY = 350; 
 
+let running = false;
+
 let fly = new Audio();
 let boom = new Audio();
-
-// document.addEventListener('keydown', function(event) {
-//     // if(lastDownTarget == canvas) {
-//     //     alert('keydown');
-//     // }
-//     pressedKey(event);
-// }, false);
 
 document.onkeydown = pressedKey;
 
 drawBG();
+ready();
 
 function drawBG() {
   
   let imageBG = new Image();
   imageBG.src = 'https://raw.githubusercontent.com/yuko70/tia-js-skuska/master/math_flappy_bg.png';
   ctx.drawImage(imageBG, 0, 0, canvasbg.width, canvasbg.height);
-
-
-
-  ready();
-
 }
 
 function ready() {
-  ctxG.font = "30px Arial";
-  ctxG.fillText("Press Enter to start", 80, 350); 
+  ctx.font = "30px Arial";
+  ctx.fillText("Press Enter to start", 80, 350); 
 }
 
 function pressedKey(ev) {
-
   let event = window.event ? window.event : ev;
-  console.log("keyDownA"); 
 
-   if (event.keyCode == '38') {
-        console.log("up");
-    }
-    else if (event.keyCode == '40') {
-        // down arrow
-        console.log("down");
-    }
+  if (event.keyCode == '38') {
+    console.log("up");
+  }
+  else if (event.keyCode == '40') {
+    console.log("down"); 
+  }
+  else if (event.keyCode == '13') {
+    console.log("enter");
+    drawBG();
+
+  }
 
 
 }
