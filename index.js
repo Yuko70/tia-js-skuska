b0ed9044835f52dd9c5d221087a06d4566c8bec1
Yuko70 <martin.masek10@gmail.com>
2019-05-29 09:27:39 +0200

    2 files modified

--- index.js
+++ index.js
@@ -2,5 +2,12 @@
 import './style.css';
 
 // Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+// const appDiv = document.getElementById('app');
+// appDiv.innerHTML = `<h1>Flappy</h1>`;
+
+
+var c = document.getElementById("bg");
+var ctx = c.getContext("2d");
+ctx.moveTo(0, 0);
+ctx.lineTo(200, 100);
+ctx.stroke(); 
